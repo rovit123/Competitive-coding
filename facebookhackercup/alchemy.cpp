37a9383bfa114aac7df6657c0c38551d2943bba6
Rovit Lawrence Fernandes <42912367+rovit123@users.noreply.github.com>
2020-08-14 10:02:38 +0530

    Update alchemy.cpp

--- facebookhackercup/alchemy.cpp
+++ facebookhackercup/alchemy.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	int t,n,a,b,z=1;
+	char *p;
+	cin>>t;
+	while(t){
+	    cin>>n;
+	    p=new char[n];
+	    for(int i=0;i<n;i++){
+	        cin>>p[i];
+	    }
+	    a=0,b=0;
+	    for(int i=0;i<n;i++){
+	        if(p[i]=='A'){
+	            a++;
+	        }
+	        else
+	            b++;
+	    }
+	    if((a-b)==1||(b-a)==1){
+	        cout<<"Case #"<<z++<<": Y"<<endl;
+	    }
+	    else
+	    cout<<"Case #"<<z++<<": N"<<endl;
+	    
+	    t--;
+	}
+	return 0;
+}
